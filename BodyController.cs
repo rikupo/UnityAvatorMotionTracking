dfa7c292375f256a6090b635712d760b5363275b
Rikupoon <47688962+rikupo@users.noreply.github.com>
2022-02-06 22:31:58 +0900

    Add UpperBody, Right Upperand Lower arm controll

--- BodyController.cs
+++ BodyController.cs
@@ -33,6 +33,11 @@ namespace BodyOperations
         Vector3 elbow_L_Vector;
         Vector3 hand_L_Vector;
         Vector3 indexDistal_L_Vector;
+        Vector3 origin_position;
+
+        Vector3 UpperBody_Vector;
+        Vector3 Spine_Vector; // Controll Upper Body
+        Vector3 Hip_Vector; // Controll All body. Base of Humanoid
 
         // UDP通信設定 (クライアント)
         const int port = 8890;
@@ -91,21 +96,50 @@ namespace BodyOperations
             AllBodyVector3D allBodyVector3D = JsonUtility.FromJson<AllBodyVector3D>(str_json);
             Debug.Log("Received Json : " + str_json);
 
-            elbow_L_Vector = new Vector3(-allBodyVector3D.elbowL.x,-allBodyVector3D.elbowL.y,-allBodyVector3D.elbowL.z);
+            //Left UpperArm #############################################################################
             upperArm_L_Vector = new Vector3(-allBodyVector3D.upperArmL.x,-allBodyVector3D.upperArmL.y,-allBodyVector3D.upperArmL.z);
-            Debug.Log("Elbow Vector : " + allBodyVector3D.elbowL.x);
-            // Rotate : 回転対象の軸が向くベクトルの設定
-            var rawVectorElbowL = Quaternion.LookRotation(elbow_L_Vector);
-            // compensate : 腕はY軸がBoneだったのでVector3.up（本来のY軸）をVector3.forward（Z軸）への回転に変換
-            var offsetElbowL = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
-            animator.GetBoneTransform(HumanBodyBones.LeftLowerArm).rotation = rawVectorElbowL * offsetElbowL;
-
             // Rotate : 回転対象の軸が向くベクトルの設定
             var rawVectorUpperArmL = Quaternion.LookRotation(upperArm_L_Vector);
             // compensate : 腕はY軸がBoneだったのでVector3.up（本来のY軸）をVector3.forward（Z軸）への回転に変換
-            var offsetUpperArmL1 = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
-            //var offsetUpperArmL2 = Quaternion.LookRotation(Vector3.up,Vector3.forward);
-            animator.GetBoneTransform(HumanBodyBones.LeftUpperArm).rotation = rawVectorUpperArmL * offsetUpperArmL1;
+            var offsetUpperArmL = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
+            animator.GetBoneTransform(HumanBodyBones.LeftUpperArm).rotation = rawVectorUpperArmL * offsetUpperArmL;
+
+            //Right UpperArm #############################################################################
+            upperArm_R_Vector = new Vector3(-allBodyVector3D.upperArmR.x,-allBodyVector3D.upperArmR.y,-allBodyVector3D.upperArmR.z);
+            var rawVectorUpperArmR = Quaternion.LookRotation(upperArm_R_Vector);
+            var offsetUpperArmR = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
+            animator.GetBoneTransform(HumanBodyBones.RightUpperArm).rotation = rawVectorUpperArmR * offsetUpperArmR;
+            
+            //Left Elbow:LowerArm #############################################################################
+            elbow_L_Vector = new Vector3(-allBodyVector3D.elbowL.x,-allBodyVector3D.elbowL.y,-allBodyVector3D.elbowL.z);
+            var rawVectorLowerArmL = Quaternion.LookRotation(elbow_L_Vector);
+            var offsetLowerArmL1 = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
+            animator.GetBoneTransform(HumanBodyBones.LeftLowerArm).rotation = rawVectorLowerArmL * offsetLowerArmL1;
+
+            //Right Elbow:LowerArm #############################################################################
+            elbow_R_Vector = new Vector3(-allBodyVector3D.elbowR.x,-allBodyVector3D.elbowR.y,-allBodyVector3D.elbowR.z);
+            var rawVectorLowerArmR = Quaternion.LookRotation(elbow_R_Vector);
+            var offsetLowerArmR1 = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
+            animator.GetBoneTransform(HumanBodyBones.RightLowerArm).rotation = rawVectorLowerArmR * offsetLowerArmR1;
+
+            //Spine #############################################################################
+            // FYI : It might be replaced for Hips after the Implementation of Leg Area had fineished.
+            UpperBody_Vector = new Vector3(allBodyVector3D.upperBody.x,allBodyVector3D.upperBody.y,allBodyVector3D.upperBody.z);
+            // UpperBody_Vector = new Vector3((float)0,(float)0.3,(float)0.1);              
+            var rawVectorUpperBody = Quaternion.LookRotation(UpperBody_Vector);
+            var offsetUpperBody = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
+            var offsetUB2 = Quaternion.LookRotation(Vector3.right,Vector3.left);
+            var offsetUB3 = Quaternion.LookRotation(Vector3.right,Vector3.left); // It llok stupid. There must be a way to 1step convertion.
+            animator.GetBoneTransform(HumanBodyBones.Spine).rotation = rawVectorUpperBody*  offsetUpperBody * offsetUB2 * offsetUB3;
+
+            //Head #############################################################################
+            //FYI : It might be replaced for Neck
+            //Spine_Vector = new Vector3(-allBodyVector3D.spine.x,-allBodyVector3D.spine.y,-allBodyVector3D.spine.z);            
+            //var rawVectorSpine = Quaternion.LookRotation(Spine_Vector);
+            //var offsetSpine = Quaternion.LookRotation(Vector3.up,Vector3.forward); // 1st argument vector to 2nd one.
+            //nimator.GetBoneTransform(HumanBodyBones.Spin).rotation = rawVectorUpperSpine * offsetSpine;
+
+
 
         }
 
@@ -117,8 +151,13 @@ namespace BodyOperations
         public class AllBodyVector3D
         {
             public EachVector wristL;
+            public EachVector wristR;
             public EachVector elbowL;
+            public EachVector elbowR;
             public EachVector upperArmL;
+            public EachVector upperArmR;
+            public EachVector upperBody;
+            
         }
 
         [Serializable]
